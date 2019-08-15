f30e1c5b48b44b0b2caad616d72741382a79a8c5
Julian Sanders <jmsander@caltech.edu>
2019-08-15 00:53:15 -0700

    Added some screenshots + sample scripts for use in user manual

---
+++ user_manual/screenshots/spirals/spacing3.py
@@ -0,0 +1,40 @@
+import pya    # Module containing KLayout API
+import numpy as np
+from chickpea import paths
+from chickpea import my_constants
+
+layout = pya.Layout()               # create new layout
+top = layout.create_cell("TOP")     # create top-level cell
+sp  = layout.create_cell("spiral")  # create cell for a delay spiral
+rib = layout.layer(1, 0)            # create strip waveguide layer
+
+horizontal = np.concatenate((
+  np.array([0, 4, 4, 8, 8]),
+  np.full(6, 8)))   # Pad to total length 9 with 8's
+
+vertical = np.concatenate((
+  np.array([16, 20, 20, 24, 24]),
+  np.full(6, 24)))  # Pad to total length 9 with 24's
+
+# Generate the spiral and instantiate its cell
+paths.delay_spiral_geo(layout, rib, sp, 
+  turns=4, 
+  spacing=2.5, 
+  vertical=vertical,
+  horizontal=horizontal,
+  horizontal_mode='symmetric',
+  vertical_mode='symmetric',
+  quad_shift=0,
+  start_turn=1, 
+  start_angle=0, 
+  end_angle=0,
+  radial_shift=4.80, 
+  n_pts=5000)
+  
+top.insert(pya.DCellInstArray(
+  sp.cell_index(),
+  pya.DTrans(pya.DVector(0, 30))
+))
+
+
+layout.write(my_constants.gds_models_path + 'test_spiral.gds')
